dee8bf5f2b5c1eb7aa79f9157ff4d893feb43e20
Ruben Dario Castilla Romero <67343176+CastRome@users.noreply.github.com>
2022-11-05 01:29:59 -0500

    test 6

--- src/app.js
+++ src/app.js
@@ -21,14 +21,12 @@ app.use(
     origin: '*',
   }),
 );
+app.configure(function () {
+  app.use(allowCrossDomain);
+});
 //app.use(allowCrossDomain);
 
 app.use(morgan('tiny'));
-
-var corsOptions = {
-  origin: 'https://recyclanet.herokuapp.com',
-  optionsSuccessStatus: 200, // some legacy browsers (IE11, various SmartTVs) choke on 204
-};
 
 //app.use(corsOptions);
 
