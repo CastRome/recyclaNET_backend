ff1716f3145d7343b38097bbbe47936f30b04b3d
Ruben Dario Castilla Romero <67343176+CastRome@users.noreply.github.com>
2022-11-05 01:23:13 -0500

    test 3

--- src/app.js
+++ src/app.js
@@ -14,13 +14,22 @@ app.use(cors());
 app.use(morgan('tiny'));
 
 var corsOptions = {
-  origin: 'https://recyclanet.herokuapp.com/',
+  origin: 'https://recyclanet.herokuapp.com',
   optionsSuccessStatus: 200, // some legacy browsers (IE11, various SmartTVs) choke on 204
 };
+var allowCrossDomain = function (req, res, next) {
+  res.header('Access-Control-Allow-Origin', 'https://recyclanet.herokuapp.com');
+  res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
+  res.header('Access-Control-Allow-Headers', 'Content-Type');
 
-app.use('/auth/local', corsOptions, userRoute);
-app.use('/api/favs', corsOptions, favsRoute);
-app.use('/api/lists', corsOptions, listsRoute);
-app.use('/api/requests', corsOptions, requestRoute);
+  next();
+};
+app.use(allowCrossDomain);
+//app.use(corsOptions);
+
+app.use('/auth/local', userRoute);
+app.use('/api/favs', favsRoute);
+app.use('/api/lists', listsRoute);
+app.use('/api/requests', requestRoute);
 
 module.exports = app;
